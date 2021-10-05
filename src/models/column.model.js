8d31eb1ebcdd301dd3227e661abb0f89039c67ad
BigZapper <dokodemodoa2112@gmail.com>
2021-10-05 19:25:48 +0700

    CRUD APIS

--- src/models/column.model.js
+++ src/models/column.model.js
@@ -1,11 +1,12 @@
 import Joi from 'joi'
 import { getDB } from '*/config/mongodb'
+import { ObjectID } from 'mongodb'
 
 // Define column collection
 const columnCollectionName = 'columns'
 const columnCollectionSchema = Joi.object({
   boardId: Joi.string().required(),
-  title: Joi.string().required().min(3).max(20),
+  title: Joi.string().required().min(3).max(20).trim(),
   cardOrder: Joi.array().items(Joi.string()).default([]),
   createdAt: Joi.date().timestamp().default(Date.now()),
   updatedAt: Joi.date().timestamp().default(null),
@@ -20,10 +21,27 @@ const createNew = async (data) => {
   try {
     const value = await validateSchema(data)
     const result = await getDB().collection(columnCollectionName).insertOne(value)
-    return result
+    return result.ops[0]
   } catch (error) {
-    console.log(error)
+    throw new Error(error)
   }
 }
 
-export const ColumnModel = { createNew }
+const update = async (id, data) => {
+  try {
+    const result = await getDB().collection(columnCollectionName).findOneAndUpdate(
+      { _id: ObjectID(id) },
+      { $set: data },
+      { returnOriginal: false }
+    )
+    console.log(result)
+    return result.value
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+export const ColumnModel = {
+  createNew,
+  update
+}
