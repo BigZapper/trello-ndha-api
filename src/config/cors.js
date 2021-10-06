0b6a7657c061134d332978219a99b84de992aa83
BigZapper <dokodemodoa2112@gmail.com>
2021-10-06 08:50:34 +0700

    Combine api fullboard, handle cors

---
+++ src/config/cors.js
@@ -0,0 +1,12 @@
+import { WHITE_LIST_DOMAINS } from '../ultilities/constants'
+
+export const corsOptions = {
+  origin: function (origin, callback) {
+    if (WHITE_LIST_DOMAINS.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error(`${origin} not allowed by CORS`))
+    }
+  },
+  optionsSuccessStatus: 200
+}
